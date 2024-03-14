c08321a1e98fa2582bfb783223b53882ecef54c6
Diwmix <sanuaburdun15@gmail.com>
2024-03-14 20:54:33 +0200

    Register token

--- accounts/views.py
+++ accounts/views.py
@@ -13,8 +13,9 @@ def register_user(request):
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
