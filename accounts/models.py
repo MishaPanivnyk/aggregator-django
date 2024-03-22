cc0cd7854c9a6d9f24931cd839feabc359dfb944
Diwmix <sanuaburdun15@gmail.com>
2024-03-22 18:08:41 +0200

    Image blank true

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from cloudinary.models import CloudinaryField
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    imageUrl = CloudinaryField("image")
+    imageUrl = CloudinaryField("image", blank=True)
     isCreator = models.BooleanField(default=False, blank=True)
     def __str__(self):
         return self.username
