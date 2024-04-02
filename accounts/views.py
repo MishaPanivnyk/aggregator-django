9f72fba66c1f718ae79f95e881c69a217a4f1eaa
Diwmix <sanuaburdun15@gmail.com>
2024-04-02 17:25:35 +0300

    fix avatar

--- accounts/views.py
+++ accounts/views.py
@@ -86,7 +86,7 @@ def user_update_profile(request):
         image = request.FILES.get('imageUrl')  # Access the uploaded image
         if image:
             uploaded_image = cloudinary.uploader.upload(image)  # Upload to Cloudinary
-            serializer.validated_data['imageUrl'] = uploaded_image['secure_url'][50:]  # Update URL
+            serializer.validated_data['imageUrl'] = uploaded_image['secure_url'][37:]  # Update URL
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
