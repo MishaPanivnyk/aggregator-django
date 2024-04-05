468f75e06dc3c0d45a8593b8b8ba499ba4f3eeb9
Diwmix <sanuaburdun15@gmail.com>
2024-04-05 20:56:32 +0300

    fix send

--- aggregator/settings.py
+++ aggregator/settings.py
@@ -112,6 +112,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'aggregator.wsgi.application'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
